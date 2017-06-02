a1226422a412eeec7b1f8dd2f8470290bd04e6e9
WIGER <1121365794@QQ.COM>
2017-06-02 16:12:28 +0800

    更新views页面、数据库的初步设计以及路由管理的初步实现

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '通讯录系统' });
+});
+router.get('/home', function(req, res, next) {
+    res.render('home', { title: '通讯录主页' });
+});
+// router.get('/login', function(req, res, next) {
+//     res.render('login', { title: '登录页' });
+// });
+router.get('/register', function(req, res, next) {
+    res.render('register', { title: '注册页' });
 });
 
 module.exports = router;
